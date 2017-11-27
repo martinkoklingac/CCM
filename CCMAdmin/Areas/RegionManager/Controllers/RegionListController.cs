d772adf0ff60b8701002ccf2639d46b4826b8389
Martin Holly <martin.holly.33@gmail.com>
2017-11-26 20:37:11 -0500

    Added basic Add(...) PUT action stub

--- CCMAdmin/Areas/RegionManager/Controllers/RegionListController.cs
+++ CCMAdmin/Areas/RegionManager/Controllers/RegionListController.cs
@@ -1,7 +1,10 @@
 using CCMAdmin.Areas.RegionManager.Models.RegionList;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Npgsql;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace CCMAdmin.Areas.RegionManager.Controllers
 {
@@ -32,7 +35,28 @@ namespace CCMAdmin.Areas.RegionManager.Controllers
 
             return View(model);
         }
+
+        [HttpPut]
+        [Route("add")]
+        public JsonResult Add([FromBody] RegionRequest request)
+        {
+            if (this.ModelState.IsValid)
+            {
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false, name = $"Error: [{ModelState["Name"].Errors.FirstOrDefault()?.ErrorMessage}]" });
+        }
     }
+
+    public class RegionRequest
+    {
+        //[JsonProperty(PropertyName = "name")]
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+    }
+
 
     public static class NpgsqlExtensions
     {
