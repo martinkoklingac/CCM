af1bfaa7529ce6ecbb304274c871a1be3b86fa9e
Martin Holly <martin.holly.33@gmail.com>
2017-11-28 00:30:47 -0500

    Implemented basic region insertion algorithm

--- SQL/SPs/insert_region.sql
+++ SQL/SPs/insert_region.sql
@@ -1,20 +1,51 @@
 CREATE OR REPLACE FUNCTION insert_region(name varchar(250), id integer = null)
-RETURNS void
+RETURNS "Regions"
 AS
 $body$
 DECLARE
-	_lb numeric := null;
-	_rb numeric := null;
+	_lb NUMERIC := NULL;
+	_rb NUMERIC := NULL;
+	_interval NUMERIC := NULL;
+	_region "Regions";
 BEGIN
 	
-	raise notice 'name : %', name;
-	raise notice 'id : %', id;
+	--raise notice 'name : %', name;
+	--raise notice 'id : %', id;
 	
 	IF EXISTS(SELECT 1 FROM public."Regions" AS r WHERE r."Id" = id) THEN
-		raise notice  'id [%] is null or Region is not present', id;
+		--raise notice  'id [%] is null or Region is not present', id;
+		
+		-- Set the left boundary
+		SELECT r."LeftBoundary" FROM public."Regions" AS r WHERE r."Id" = id
+		INTO _lb;
+		
+		-- Set the right boundary
+		-- We will insert children from the left by default
+		SELECT
+			COALESCE((
+				SELECT MIN(rc."LeftBoundary") FROM "Regions" AS rc 
+				WHERE rc."LeftBoundary" BETWEEN rp."LeftBoundary" AND rp."RightBoundary"
+				AND rc."Id" <> rp."Id"), 
+				rp."RightBoundary")
+		FROM "Regions" AS rp
+		WHERE rp."Id" = id
+		INTO _rb;
 		
 		
+		--RAISE NOTICE '_lb = %', _lb;
+		--RAISE NOTICE '_rb = %', _rb;
 		
+		SELECT ((_rb - _lb) / 3)
+		INTO _interval;
+		
+		SELECT _lb + _interval
+		INTO _lb;
+		
+		SELECT _rb - _interval
+		INTO _rb;
+		
+		--RAISE NOTICE '- _lb = %', _lb;
+		--RAISE NOTICE '- _rb = %', _rb;
 		
 	ELSE
 		
@@ -23,13 +54,14 @@ BEGIN
 		
 		_rb = _lb + 1;
 		
-		RAISE NOTICE '_lb = %', _lb;
-		RAISE NOTICE '_rb = %', _rb;
+		--RAISE NOTICE '_lb = %', _lb;
+		--RAISE NOTICE '_rb = %', _rb;
 		
-		
-		INSERT INTO "Regions"("Name", "LeftBoundary", "RightBoundary") VALUES(name, _lb, _rb);
 	END IF;
 	
+	INSERT INTO "Regions"("Name", "LeftBoundary", "RightBoundary") 
+	VALUES(name, _lb, _rb) RETURNING * INTO _region;
+	RETURN _region;
 END;
 $body$
 LANGUAGE plpgsql
