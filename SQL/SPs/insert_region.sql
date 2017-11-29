b77d93e3ac1349f7e1e115af906224bc274049cf
Martin Holly <martin.holly.33@gmail.com>
2017-11-29 16:47:24 -0500

    Renamed table Regions -> regions

--- SQL/SPs/insert_region.sql
+++ SQL/SPs/insert_region.sql
@@ -1,34 +1,34 @@
-CREATE OR REPLACE FUNCTION insert_region(name varchar(250), id integer = null)
-RETURNS "Regions"
+CREATE OR REPLACE FUNCTION insert_region(name varchar(250), parent_id integer = null)
+RETURNS regions
 AS
 $body$
 DECLARE
 	_lb NUMERIC := NULL;
 	_rb NUMERIC := NULL;
 	_interval NUMERIC := NULL;
-	_region "Regions";
+	_region regions;
 BEGIN
 	
 	--raise notice 'name : %', name;
 	--raise notice 'id : %', id;
 	
-	IF EXISTS(SELECT 1 FROM public."Regions" AS r WHERE r."Id" = id) THEN
+	IF EXISTS(SELECT 1 FROM regions AS r WHERE r.id = parent_id) THEN
 		--raise notice  'id [%] is null or Region is not present', id;
 		
 		-- Set the left boundary
-		SELECT r."LeftBoundary" FROM public."Regions" AS r WHERE r."Id" = id
+		SELECT r.left_bound FROM regions AS r WHERE r.id = parent_id
 		INTO _lb;
 		
 		-- Set the right boundary
 		-- We will insert children from the left by default
 		SELECT
 			COALESCE((
-				SELECT MIN(rc."LeftBoundary") FROM "Regions" AS rc 
-				WHERE rc."LeftBoundary" BETWEEN rp."LeftBoundary" AND rp."RightBoundary"
-				AND rc."Id" <> rp."Id"), 
-				rp."RightBoundary")
-		FROM "Regions" AS rp
-		WHERE rp."Id" = id
+				SELECT MIN(rc.left_bound) FROM regions AS rc 
+				WHERE rc.left_bound BETWEEN rp.left_bound AND rp.right_bound
+				AND rc.id <> rp.id), 
+				rp.right_bound)
+		FROM regions AS rp
+		WHERE rp.id = parent_id
 		INTO _rb;
 		
 		
@@ -49,7 +49,7 @@ BEGIN
 		
 	ELSE
 		
-		SELECT COALESCE(MAX(r."RightBoundary"), -1) + 1 FROM "Regions" AS r
+		SELECT COALESCE(MAX(r.right_bound), -1) + 1 FROM regions AS r
 		INTO _lb;
 		
 		_rb = _lb + 1;
@@ -59,9 +59,10 @@ BEGIN
 		
 	END IF;
 	
-	INSERT INTO "Regions"("Name", "LeftBoundary", "RightBoundary") 
+	INSERT INTO regions(name, left_bound, right_bound) 
 	VALUES(name, _lb, _rb) RETURNING * INTO _region;
 	RETURN _region;
+	
 END;
 $body$
 LANGUAGE plpgsql
