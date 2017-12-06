786fbb24d6e26fc749fb78350456673338bbec81
Martin Holly <martin.holly.33@gmail.com>
2017-12-06 17:52:03 -0500

    Added UnitOfWorkConfig class for DI

---
+++ CCMAdmin/UnitOfWorkConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace CCMAdmin
+{
+    public interface IUnitOfWorkConfig
+    {
+        string ConnectionString { get; }
+    }
+
+    public class UnitOfWorkConfig :
+        IUnitOfWorkConfig
+    {
+        public UnitOfWorkConfig(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; }
+    }
+}
