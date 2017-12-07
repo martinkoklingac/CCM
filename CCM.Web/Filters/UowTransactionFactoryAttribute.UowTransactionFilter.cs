2c05af7034607c3a9e6f13bc44ea1eff2f0eb008
Martin Holly <martin.holly.33@gmail.com>
2017-12-07 17:47:26 -0500

    Added couple of projects to CCM.sln - CCM.Data for DB access - CCM.Data.Web for managing connections for requests

---
+++ CCM.Web/Filters/UowTransactionFactoryAttribute.UowTransactionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace CCM.Data.Web.Filters
+{
+    public partial class UowTransactionFactoryAttribute
+    {
+        #region INTERNAL TYPES
+        private class UowTransactionFilter :
+            IAsyncActionFilter
+        {
+            private readonly ILogger<UowTransactionFilter> _logger;
+            public UowTransactionFilter(ILogger<UowTransactionFilter> logger)
+            {
+                _logger = logger;
+            }
+
+            public async Task OnActionExecutionAsync(
+                ActionExecutingContext context, 
+                ActionExecutionDelegate next)
+            {
+                this._logger.LogTrace("before action");
+                var resultContext = await next();
+                this._logger.LogTrace("after action");
+            }
+        }
+        #endregion
+    }
+}
---
+++ CCM.Web/Filters/UowTransactionFactoryAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CCM.Data.Web.Filters
+{
+    public partial class UowTransactionFactoryAttribute :
+        Attribute,
+        IFilterFactory
+    {
+        #region CONSTRUCTORS
+        public UowTransactionFactoryAttribute() : 
+            base()
+        { }
+        #endregion
+
+        #region PUBLIC PROPERTIES
+        public bool IsReusable => false;
+        #endregion
+
+        #region PUBLIC METHODS
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            var logger = (ILogger<UowTransactionFilter>)serviceProvider
+                .GetService(typeof(ILogger<UowTransactionFilter>));
+            return new UowTransactionFilter(logger);
+        }
+        #endregion
+    }
+}
