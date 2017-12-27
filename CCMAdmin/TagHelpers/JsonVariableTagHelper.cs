5493f366c489e0befa26c0ca526d6910c36c6030
Martin Holly <martin.holly.33@gmail.com>
2017-12-27 15:42:39 -0500

    Added json-variable tag helper

---
+++ CCMAdmin/TagHelpers/JsonVariableTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Newtonsoft.Json;
+
+namespace CCMAdmin.TagHelpers
+{
+    [HtmlTargetElement("json-variable", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class JsonVariableTagHelper :
+        TagHelper
+    {
+        #region PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public object Data { get; set; }
+        #endregion
+
+        #region PUBLIC METHODS
+        public override void Process(
+            TagHelperContext context,
+            TagHelperOutput output)
+        {
+            var json = JsonConvert
+                .SerializeObject(this.Data);
+
+            output.TagName = "script";
+            output.Attributes.Add("type", "text/javascript");
+            output.Content.SetHtmlContent($"let {this.Name} = {json};");
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+        #endregion
+    }
+}
