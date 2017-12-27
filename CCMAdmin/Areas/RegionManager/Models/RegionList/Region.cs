151ca7b82e807328e23c3c249255d719af026135
Martin Holly <martin.holly.33@gmail.com>
2017-12-27 17:22:26 -0500

    Using json-variable helper to create the regions JSON model

--- CCMAdmin/Areas/RegionManager/Models/RegionList/Region.cs
+++ CCMAdmin/Areas/RegionManager/Models/RegionList/Region.cs
@@ -1,12 +1,15 @@
 ﻿using CCM.Data.SchemaUtils;
+using Newtonsoft.Json;
 
 namespace CCMAdmin.Areas.RegionManager.Models.RegionList
 {
     public class Region
     {
         [Meta(Name = "id")]
+        [JsonProperty("id")]
         public int Id { get; set; }
         [Meta(Name = "name")]
+        [JsonProperty("name")]
         public string Name { get; set; }
     }
 }
