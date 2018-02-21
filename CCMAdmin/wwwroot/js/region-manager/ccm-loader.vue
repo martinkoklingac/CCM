1c7a4160f1e97be902ca81cc5697e4f40e0120f6
Martin Holly <martin.holly.33@gmail.com>
2018-02-21 16:52:36 -0500

    Broke out ccm-loader component

---
+++ CCMAdmin/wwwroot/js/region-manager/ccm-loader.vue
@@ -0,0 +1,12 @@
+﻿<template>
+    <div class="loader">
+        <span class="loader-item"></span>
+        <span class="loader-item"></span>
+        <span class="loader-item"></span>
+        <span class="loader-item"></span>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
