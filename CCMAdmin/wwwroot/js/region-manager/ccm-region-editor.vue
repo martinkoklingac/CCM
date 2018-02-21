2a7e8c7dd6ed65d0c9edba965e7d46fe2f85e6da
Martin Holly <martin.holly.33@gmail.com>
2018-02-21 16:19:27 -0500

    Broke out ccm-region-editor vue component

---
+++ CCMAdmin/wwwroot/js/region-manager/ccm-region-editor.vue
@@ -0,0 +1,20 @@
+﻿<template>
+    <div class="ccm-region-editor">
+        <div>Region Editor</div>
+        <div>For : [{{id}}]</div>
+        <div>
+            <button v-on:click="onCancel" class="ccm-btn normal cancel-btn">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['id'],
+        methods: {
+            onCancel: function () {
+                this.$emit("cancel");
+            }
+        }
+    }
+</script>
