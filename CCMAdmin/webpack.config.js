bf97bce73ac8ba92c319eb2eb96b239b73ddda09
Martin Holly <martin.holly.33@gmail.com>
2018-02-22 11:36:01 -0500

    Added webpack as a build step

---
+++ CCMAdmin/webpack.config.js
@@ -0,0 +1,33 @@
+﻿/// <binding BeforeBuild='Run - Production, Run - Development' Clean='Run - Development' />
+"use strict";
+
+module.exports = {
+    entry: "./wwwroot/js/main.js",
+    output: {
+        filename: "./wwwroot/js/out.js"
+    },
+    //devServer: {
+    //    contentBase: ".",
+    //    host: "localhost",
+    //    port: 9000
+    //},
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue?$/,
+                loader: "vue-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    }
+};
---
+++ CCMAdmin/wwwroot/js/main.js
@@ -0,0 +1,48 @@
+﻿import Vue from 'vue';
+import CcmLoader from './region-manager/ccm-loader.vue';
+import CcmRegionDeleter from './region-manager/ccm-region-deleter.vue';
+import CcmRegionEditor from './region-manager/ccm-region-editor.vue';
+import CcmRegionConstructor from './region-manager/ccm-region-constructor.vue';
+import CcmRegion from './region-manager/ccm-region.vue';
+
+//new Vue({
+//    el: '#_x',
+//    components: {
+//        CcmLoader,
+//        CcmRegionDeleter
+//    }
+//})
+
+new Vue({
+    el: '#_regionList',
+    components: {
+        CcmLoader,
+        CcmRegionDeleter,
+        CcmRegionEditor,
+        CcmRegionConstructor,
+        CcmRegion
+    },
+    data: {
+        regions: regions
+    },
+    methods: {
+        onRegionCreated: function (region) {
+            this.regions.push(region);
+        },
+
+        onRegionDeleted: function (d) {
+            let f = function (x) {
+                return x.id === d.deletedId;
+            };
+
+            let i = this.regions.findIndex(f);
+
+            if (i >= 0) {
+                this.regions.splice(i, 1);
+            }
+
+            this.regions = this.regions
+                .concat(d.promotedRegions);
+        }
+    }
+});
